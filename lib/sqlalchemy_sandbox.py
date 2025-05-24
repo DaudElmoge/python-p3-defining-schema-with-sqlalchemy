4df73a52eafe4c99583e6cfef8493aa5bcb038ca
Daud Abdiwahab <daudwahab1320@gmail.com>
2025-05-24 19:05:21 +0300

    done

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -5,8 +5,12 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
-class Student(Base):
-    pass
+class Student(Base):#Student is a Python class that represents a table named students.
+    __tablename__ = "students"#__tablename__ = 'students' tells SQLAlchemy what to call the actual table
+    id =Column(Integer(),primary_key=True)
+    name = Column(String())
 
 if __name__ == '__main__':
-    pass
+    engine = create_engine ("sqlite:///students.db")
+    Base.metadata.create_all(engine)#create_engine('sqlite:///students.db') creates a connection to a new (or existing) SQLite file-based database called students.db.
+    #This block runs only when the script is executed directly (python lib/sqlalchemy_sandbox.py).
